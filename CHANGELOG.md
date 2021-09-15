64e0f96163bc726f4a8b7fe2b9b59f7bee57b9a4
xzp <xingzepeng@geo-compass.com>
2021-09-15 15:18:29 +0800

    feat(首页): 运用react-transition-group给首页的菜单，增加动画

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,17 +1,24 @@
-# 1.0.0 (2021-09-13)
+# 1.0.0 (2021-09-15)
 
 
 ### Bug Fixes
 
+* **替换logo:** 替换logo ([af856ee](https://github.com/xzp-git/React-ts-hooks/commit/af856ee5a75a51ec28c1fd60e7458963bd14e8c4))
 * **commitlint:** commitlint ([4e5aea1](https://github.com/xzp-git/React-ts-hooks/commit/4e5aea1696138574afc980a148149ed9b0bfd50d))
+* **eslint配置:** 修改eslint配置 ([49f9e3a](https://github.com/xzp-git/React-ts-hooks/commit/49f9e3a551b4b3bafe1c32b2bc8b88ec166a39e1))
+* **husky:** 修复提交时husky不起作用的问题| ([7ecc066](https://github.com/xzp-git/React-ts-hooks/commit/7ecc0662f7daafb5d1d3a9a57d82675d2be230d0))
+* **husky:** 修复husky不起作用的问题 ([c25e8e4](https://github.com/xzp-git/React-ts-hooks/commit/c25e8e462077db202cee876b558e98812363c182))
 * **husky:** husky 重新安装 ([23f1abe](https://github.com/xzp-git/React-ts-hooks/commit/23f1abecfb66c99dad83c6cd70d01d6bdd95c7fe))
 
 
 ### Features
 
+* **底部导航:** 底部导航切换 ([2817ee8](https://github.com/xzp-git/React-ts-hooks/commit/2817ee8bdeb84a8462101de4816ce20542f7372a))
+* **首页顶部菜单:** 首页顶部菜单 ([68b1b64](https://github.com/xzp-git/React-ts-hooks/commit/68b1b64cf2269f29a32ff60734f6e53791bf35ec))
 * **changelog:** 生成变更日志文件 ([485cf91](https://github.com/xzp-git/React-ts-hooks/commit/485cf9111b9afcf30d92b4d593545f8b771b21af))
 * **commitizen 配置:** 配置git提交规范，commitizen ([7d18c5c](https://github.com/xzp-git/React-ts-hooks/commit/7d18c5c3015da5ff8137b7749401c4792424a78d))
 * **lint-staged:** lint-staged配置 ([2b24491](https://github.com/xzp-git/React-ts-hooks/commit/2b24491631d86f33e241180cab673e6070f4e4e6))
+* **reduer:** 编写各个模块的reducer ([a9de153](https://github.com/xzp-git/React-ts-hooks/commit/a9de15359779574cd1bb85aedd35eb62520fae77))
 
 
 
--- src/routes/Home/components/HomeHeader/index.tsx
+++ src/routes/Home/components/HomeHeader/index.tsx
@@ -1,13 +1,34 @@
 import React from 'react'
-
 import {BarsOutlined} from '@ant-design/icons'
 import logo from '@/assets/images/logo.png'
-
+import { Transition } from 'react-transition-group'
 import classname from 'classnames'
 import './index.less'
+
 interface Props{
     currentCategory:string; //表示当前选中的分类，这个值会被存放在redux中
     setCurrentCategory:(currentCategory:string)=>void
+}
+
+const duration = 1000 //动画持续的时间
+const defaultStyle = { //默认样式
+  transition:`opacity ${duration}ms ease-in-out`,
+  opacity:0
+}
+
+interface TransitionStyles{
+  entering:React.CSSProperties,
+  entered:React.CSSProperties,
+  exiting:React.CSSProperties,
+  exited:React.CSSProperties
+}
+
+
+const transitionStyles:TransitionStyles = {
+  entering:{opacity:1},
+  entered:{opacity:1},
+  exiting:{opacity:0},
+  exited:{opacity:0}
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
@@ -28,28 +49,44 @@ function HomeHeader(props:Props) {
         <img src={logo} alt="" />
         <BarsOutlined onClick={() => setIsMenuVisible(!isMenuVisible)} />
       </div>
-      {
-        isMenuVisible && <ul
-          className="category"
-          onClick={setCurrentCategory}
-        >
-          <li data-category="all" className={
-            classname({
-              active:props.currentCategory === 'all'
-            })
-          }>全部课程</li>
-          <li data-category="react" className={
-            classname({
-              active:props.currentCategory === 'react'
-            })
-          }>经济金融</li>
-          <li data-category="vue" className={
-            classname({
-              active:props.currentCategory === 'vue'
-            })
-          }>自然科学</li>
-        </ul>
-      }
+      <Transition
+        in={isMenuVisible}
+        timeout={duration}
+      >
+        {
+          (state:keyof TransitionStyles) => {
+            return (
+              <ul
+                className="category"
+                onClick={setCurrentCategory}
+                style={
+                  {
+                    ...defaultStyle,
+                    ...transitionStyles[state]
+                  }
+                }
+              >
+                <li data-category="all" className={
+                  classname({
+                    active:props.currentCategory === 'all'
+                  })
+                }>全部课程</li>
+                <li data-category="react" className={
+                  classname({
+                    active:props.currentCategory === 'react'
+                  })
+                }>经济金融</li>
+                <li data-category="vue" className={
+                  classname({
+                    active:props.currentCategory === 'vue'
+                  })
+                }>自然科学</li>
+              </ul>
+            )
+          }
+        }
+      </Transition>
+      
       
     </header>
   )
